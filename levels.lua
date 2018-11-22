bd07ae9500cd4bac2cefba0352d5eae972f7af07
maxmol <maxmolyt@gmail.com>
2018-11-22 15:09:13 +0300

    Update levels.lua

--- levels.lua
+++ levels.lua
@@ -2,14 +2,19 @@
 levels = {}
 
 function levels:draw()
+    if self.level and self.level.draw then
+        self.level:draw()
+    end
 end
 
-function levels:update()
+function levels:update(dt)
+    if self.level and levels.level.update then
+        self.level:update(dt)
+    end
 end
 
 function levels:load(name)
     local level = require('levels/' .. name)
     if level.load then level:load() end
-    self.draw = level.draw or self.draw
-    self.update = level.update or self.update
+    self.level = level
 end
