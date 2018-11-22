cedfa876eabc926534b64a8953f1d0288f07953a
maxmol <maxmolyt@gmail.com>
2018-11-22 11:08:33 +0300

    whoops2

--- levels.lua
+++ levels.lua
@@ -8,7 +8,7 @@ function levels:update()
 end
 
 function levels:load(name)
-    local level = require('levels/' .. name .. '.lua')
+    local level = require('levels/' .. name)
     if level.load then level:load() end
     self.draw = level.draw or self.draw
     self.update = level.update or self.update
