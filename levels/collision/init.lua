bdf8d943031a3a5ca03fbfc558b6859687f5f3fe
maxmol <maxmolyt@gmail.com>
2018-12-06 22:27:54 +0300

    collision

---
+++ levels/collision/init.lua
@@ -0,0 +1,23 @@
+local _ = {}
+
+function _:load()
+
+end
+
+function _:update(dt)
+
+end
+
+function _:draw()
+
+end
+
+function _:exit()
+
+end
+
+function _:mousepressed(x, y, button, istouch, presses)
+
+end
+
+return _
